9fef5a09e2840d12ad2c8a2cc0dce5fbc1d4337b
YoungBeom Kim <darania@Kookmin.ac.kr>
2023-11-08 01:26:25 +0900

    [231108]-[Wire]-[Completed]

---
+++ Circuits/Combinational Logic/Basic Gates/Wire.v
@@ -0,0 +1,23 @@
+//============================================================================
+//
+//  AUTHOR      : YoungBeom Kim
+//  SPEC        :
+//  HISTORY     : 2023-11-08 오전 12:13:28
+//
+//      Copyright (c) 2023 Crypto Optimization & Application LAb. MIT license
+//
+//============================================================================
+
+
+//Problem : Implement the wire following circuit:
+
+module top_module
+(
+	input	in	,
+		
+	output	out
+);
+
+assign	out = in;
+
+endmodule
