3ac614a7fca18b71454f1d0258c7fcf11e3d43ab
YoungBeom Kim <darania@Kookmin.ac.kr>
2024-10-02 18:39:19 +0900

    updates Getting Started

---
+++ 1_Getting Started/Getting Started.v
@@ -0,0 +1,18 @@
+//============================================================================
+//
+//  AUTHOR      : YoungBeom Kim
+//  SPEC        :
+//  HISTORY     : 2024-10-02 오후 6:36:58
+//
+//      Copyright (c) 2023 Crypto Optimization & Application LAb. MIT license
+//
+//============================================================================
+
+module top_module
+( 
+	output one 
+);
+
+    assign one = 1;
+
+endmodule
---
+++ 1_Getting Started/Output Zero.v
@@ -0,0 +1,18 @@
+//============================================================================
+//
+//  AUTHOR      : YoungBeom Kim
+//  SPEC        :
+//  HISTORY     : 2024-10-02 오후 6:39:02
+//
+//      Copyright (c) 2023 Crypto Optimization & Application LAb. MIT license
+//
+//============================================================================
+
+module top_module
+( 
+	output zero 
+);
+
+    assign zero = 0;
+
+endmodule
